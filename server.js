b4a516209eba391768720c2dc8be7c0e8565fdf6
vandu1979 <vandana_kothari@yahoo.com>
2022-12-16 14:54:00 -0700

    Create

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 
 
 // Middleware
 app.set('view engine', 'jsx');
 app.engine('jsx',require('express-react-views').createEngine());
+app.use(express.urlencoded({extended:false}));
+
+
+
+// Connect to Mongodb  / remove deprication
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', ()=>{
+  console.log('connected to mongo');
+})
+
 
 app.get('/logs', (req, res)=>{
     res.send('Welcome to Captains_logs');
@@ -13,6 +28,18 @@ app.get('/logs', (req, res)=>{
 // NEW ROUTE
 app.get('/logs/new', (req, res)=>{
     res.render('New');
+})
+
+// POST //Create
+app.post('/logs', (req, res)=>{
+    console.log(req.body)
+    res.send('received')
+
+if(req.body.shipIsBroken === 'on'){
+    req.body.shipIsBroken = true;
+} else {
+    req.body.shipIsBroken = false
+}
 })
 
 
